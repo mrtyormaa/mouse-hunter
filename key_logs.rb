df8cf7176253f13e8695599ec54d6beb6fb1a89c
mrtyormaa <asutosh.satapathy@gmail.com>
2015-05-18 10:06:00 -0400

    changes

--- key_logs.rb
+++ key_logs.rb
@@ -4,6 +4,7 @@
 require 'Win32API'
 class Keys_To_Capture
   attr_reader :char, :total_time, :counter
+
   def initialize(char)
     @total_time = 0
     @char = char
@@ -29,10 +30,10 @@ class Key_Logs
     break_check = 'I' #Some junk
 
     files = open_file
-    #we are keeping track of only A, S, D and Q: Quit
+    #we are keeping track of only A, B, C and Q: Quit
     characters = Array.new
-    find_list = [0x41, 0x44, 0x53, 0x51]
-    find_list.each { |var| characters << Keys_To_Capture.new(var)}
+    find_list = [0x41, 0x42, 0x43, 0x51]
+    find_list.each { |var| characters << Keys_To_Capture.new(var) }
     #characters = Keys_To_Capture.new([0x41, 0x44, 0x53, 0x51])
 
     until break_check.eql?('Q')
@@ -45,7 +46,7 @@ class Key_Logs
       characters.each do |key| #letters
         if nave.call(key.char) & 0x01 == 1
           break_check = key.char.chr
-          if find_list.any? {|i| key.char.chr.include?(i.chr)}
+          if find_list.any? { |i| key.char.chr.include?(i.chr) }
             if key.counter == 1
               start_time = Time.now
               key.instance_variable_set('@counter', 2)
@@ -67,9 +68,57 @@ class Key_Logs
     end
 
   end
+
+  def capture_single_click
+
+    nave = Win32API.new('user32', 'GetAsyncKeyState', ['i'], 'i')
+
+    start_time = Time.now
+    break_check = 'I' #Some junk
+    first_time = 1
+    temp_key = nil
+
+    files = open_file
+    #we are keeping track of only A, B, C and Q: Quit
+    characters = Array.new
+    find_list = [0x41, 0x42, 0x43, 0x51]
+    find_list.each { |var| characters << Keys_To_Capture.new(var) }
+    #characters = Keys_To_Capture.new([0x41, 0x44, 0x53, 0x51])
+
+    until break_check.eql?('Q')
+      # (0x30..0x39).each do |num1| #numbers
+      #   if nave.call(num1) & 0x01 == 1
+      #     save_file(files, num1.chr)
+      #   end
+      # end
+
+      characters.each do |key| #letters
+        if nave.call(key.char) & 0x01 == 1
+          break_check = key.char.chr
+          if find_list.any? { |i| key.char.chr.include?(i.chr) }
+            if first_time == 1
+              first_time += 1
+              start_time = Time.now
+              temp_key = key
+            end
+            save_file(files, temp_key.char.chr + ' ' + (Time.now - start_time).to_s)
+            temp_key.instance_variable_set('@total_time', temp_key.total_time + (Time.now - start_time))
+            start_time = Time.now
+            temp_key = key
+          end
+        end
+      end
+    end
+
+    #note the total time for each characters
+    characters.each do |key|
+      save_file(files, key.char.chr + ' - Total Time: ' +key.total_time.to_s)
+    end
+
+  end
 end
 
 
-Key_Logs.new.capture_data #Start the key-logger
+Key_Logs.new.capture_single_click #Start the key-logger
 
 # ¿ The End ?
